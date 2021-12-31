1b919d8ea34504ef71fc27eca804a7199c479199
Sivasubramanian velusamy <sivavelusamy28@gmail.com>
2021-12-30 18:33:47 -0600

    Moving output variables to output.tf

--- main.tf
+++ main.tf
@@ -24,12 +24,3 @@ resource "aws_instance" "app_server" {
 
 }
 
-output "aws_ec2_ip"{
-  description = "public ip of the EC2 instance"
-  value = aws_instance.app_server.public_ip
-}
-
-output "aws_ec2_ami"{
-  description = "AMI of the EC2 instance"
-  value = aws_instance.app_server.ami
-}
